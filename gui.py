805070f8cd49d0916f0560f51d1e032276e86fa8
SeanGraney <sgraney@hawk.iit.edu>
2021-11-30 11:28:19 -0600

    Initial gui file. Run gui.py to begin

---
+++ gui.py
@@ -0,0 +1,16 @@
+import tkinter as tk   
+root = tk.Tk()
+
+def main():
+    pass    
+
+
+
+
+# creates new window
+def create_window():
+    window = tk.Toplevel(root)
+
+# ---------------------------------Keep At End---------------------------
+main()
+root.mainloop()
