1c443b5e5d64f7bc315ada682500a2afc531e7bd
SeanGraney <sgraney@hawk.iit.edu>
2021-11-30 19:57:32 -0600

    Adding login gui

--- gui.py
+++ gui.py
@@ -1,15 +1,39 @@
-import tkinter as tk   
-root = tk.Tk()
+from tkinter import*  
+
+# Create Root Window
+root = Tk()
+
+login = False
+
 
 def main():
-    pass    
+    
+    # ----------------login screen-------------------
+    if(not login):
+        # Open window with dimension AxA
+        root.geometry('250x100')
 
+        frameLog = Frame(root)
+        frameLog.grid(row=0, column=2) 
 
+        Label(frameLog, text='Login').grid(row=0)
+        Label(root, text='Username').grid(row=1)
+        Label(root, text='Password').grid(row=2)
 
+        e1 = Entry(root)
+        e2 = Entry(root)
+        e1.grid(row=1, column=2)
+        e2.grid(row=2, column=2)
+
+        loginButton = Button(root, text='Login')
+        loginButton.grid(row=3, column=2)
+        
+
+# Create login window
 
 # creates new window
 def create_window():
-    window = tk.Toplevel(root)
+    window = Toplevel(root)
 
 # ---------------------------------Keep At End---------------------------
 main()
