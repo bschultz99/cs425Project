5ab044a4b79372d39529439961d812d8cd9d0ebf
JardTheBard <jared.wagler@gmail.com>
2021-12-01 10:53:36 -0600

    Added callable capability

--- gui.py
+++ gui.py
@@ -36,5 +36,6 @@ def create_window():
     window = Toplevel(root)
 
 # ---------------------------------Keep At End---------------------------
-main()
-root.mainloop()
+if __name__ == '__main__':
+    main()
+    root.mainloop()
